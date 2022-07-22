3ea4186fce66832029096e9d963c4e659e7a7b6a
Leonardo Iplinsky <leoipy16@gmail.com>
2022-07-22 11:53:55 -0300

    Update projetoSemanal.sql

--- projetoSemanal.sql
+++ projetoSemanal.sql
@@ -1,29 +1,7 @@
-CREATE DATABASE  IF NOT EXISTS `resilia` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE  IF NOT EXISTS `resilia`;
 USE `resilia`;
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
---
--- Host: localhost    Database: resilia
--- ------------------------------------------------------
--- Server version	5.7.36
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `alunos`
---
 
 DROP TABLE IF EXISTS `alunos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alunos` (
   `cpf` varchar(11) NOT NULL,
   `nome` varchar(120) NOT NULL,
@@ -33,49 +11,26 @@ CREATE TABLE `alunos` (
   PRIMARY KEY (`cpf`),
   KEY `fk_turmas` (`id_turma`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `alunos`
---
 
 LOCK TABLES `alunos` WRITE;
-/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
 INSERT INTO `alunos` VALUES ('06734218711','Leonardo','leoipy16@gmail.com',1,1),('63514948026','Ana Beatriz','anana@gmail.com',2,2),('63723935163','Vitor','vita1@gmail.com',1,1);
-/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `cursos`
---
-
 DROP TABLE IF EXISTS `cursos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `cursos` (
   `id_curso` int(11) NOT NULL,
   `tipo` varchar(70) NOT NULL,
   PRIMARY KEY (`id_curso`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cursos`
---
 
 LOCK TABLES `cursos` WRITE;
-/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
 INSERT INTO `cursos` VALUES (1,'WebDev FullStack'),(2,'Data Analytics');
-/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `entregas`
---
 
 DROP TABLE IF EXISTS `entregas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `entregas` (
   `cpf` varchar(11) NOT NULL,
   `link` varchar(300) NOT NULL,
@@ -83,25 +38,13 @@ CREATE TABLE `entregas` (
   `conceito` varchar(30) DEFAULT NULL,
   KEY `fk_alunos` (`cpf`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `entregas`
---
 
 LOCK TABLES `entregas` WRITE;
-/*!40000 ALTER TABLE `entregas` DISABLE KEYS */;
+
 INSERT INTO `entregas` VALUES ('06734218711','projetoleo.com.br',4,'Chegando lá'),('63514948026','projetoana.com.br',5,'Pronto'),('06734218711','projetoleom1.com.br',1,'Chegando lá'),('63514948026','projetoanam1.com.br',1,'Pronto'),('06734218711','projetoleom2.com.br',2,'Mais que pronto'),('63514948026','projetoanam2.com.br',2,'Mais que pronto'),('06734218711','projetoleom3.com.br',3,'Ainda não está pronto'),('63514948026','projetoanam3.com.br',3,'Mais que pronto');
-/*!40000 ALTER TABLE `entregas` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `turmas`
---
-
 DROP TABLE IF EXISTS `turmas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `turmas` (
   `id_turma` int(11) NOT NULL,
   `nome_turma` varchar(30) NOT NULL,
@@ -109,25 +52,8 @@ CREATE TABLE `turmas` (
   PRIMARY KEY (`id_turma`),
   KEY `fk_cursos` (`id_curso`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `turmas`
---
-
 LOCK TABLES `turmas` WRITE;
-/*!40000 ALTER TABLE `turmas` DISABLE KEYS */;
+
 INSERT INTO `turmas` VALUES (1,'Geração Futuro',1),(2,'Love Statistics',2);
-/*!40000 ALTER TABLE `turmas` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-07-22 11:49:34
